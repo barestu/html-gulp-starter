5167fa8cf18b49661bfc4416bc805b5d3a6a3501
Fandy Barestu <fandy@softwareseni.com>
2020-10-11 14:55:32 +0700

    Update config

---
+++ README.md
@@ -0,0 +1,16 @@
+# How to use
+
+1. Install dependencies
+```
+npm install
+```
+
+2. Build project
+```
+npm run build
+```
+
+3. Develop
+```
+npm run dev
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,91 @@
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
-const uglifycss = require('gulp-uglifycss');
-const concat = require('gulp-concat');
-const rename = require('gulp-rename');
-const fileinclude = require('gulp-file-include');
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify");
+const uglifycss = require("gulp-uglifycss");
+const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const fileinclude = require("gulp-file-include");
+const concatCss = require("gulp-concat-css");
 
 // Copy the html file into dist folder
-function buildHtml() {
-  return src('src/html/*.html')
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(dest('dist'));
+function html() {
+  return src("src/*.html")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(dest("dist"));
 }
 
-// Optimize image file
-function imageMin() {
-  return src('src/images/*')
-    .pipe(imagemin())
-    .pipe(dest('dist/images'))
-}
-
-// Minify JS
-function compileJs() {
-  return src('src/js/**/*.js')
-    .pipe(concat('main.js'))
+// Bundle & uglify app js
+function js() {
+  return src("src/js/**/*.js")
+    .pipe(concat("app.js"))
     .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(dest('dist/js'))
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(dest("dist/assets/js"))
     .pipe(browserSync.stream());
+}
+
+// Bundle & uglify js plugins
+function jsPlugins() {
+  return src([
+    "node_modules/jquery/dist/jquery.min.js",
+    "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+    "node_modules/owl.carousel/dist/owl.carousel.min.js",
+  ])
+    .pipe(concat("plugins.js"))
+    .pipe(uglify())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(dest("dist/assets/js"));
 }
 
 // Compile sass into minified css
-function compileCss() {
-  return src('src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+function css() {
+  return src("src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(uglifycss({ uglyComments: true }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(dest('dist/css'))
+    .pipe(rename({ basename: 'styles', extname: ".min.css" }))
+    .pipe(dest("dist/assets/css"))
     .pipe(browserSync.stream());
+}
+
+// Bundle & uglify css plugins
+function cssPlugins() {
+  return src([
+    "node_modules/owl.carousel/dist/assets/owl.carousel.min.css",
+    "node_modules/owl.carousel/dist/assets/owl.theme.default.min.css"
+  ])
+    .pipe(concatCss('plugins.min.css'))
+    .pipe(uglifycss({ uglyComments: true }))
+    .pipe(dest("dist/assets/css"));
 }
 
 // Watch for any src file changes
 function livereload() {
   browserSync.init({
     server: {
-      baseDir: 'dist/'
-    }
+      baseDir: "dist/",
+    },
   });
 
-  watch('src/scss/**/*.scss', compileCss);
-  watch('src/js/**/*.js', compileJs).on('change', browserSync.reload);
-  watch('src/images/*', imageMin);
-  watch('src/**/*.html', buildHtml).on('change', browserSync.reload);
+  watch("src/css/**/*.css", css);
+  watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", js).on("change", browserSync.reload);
+  watch("src/**/*.html", html).on("change", browserSync.reload);
 }
 
 exports.default = series(
-  buildHtml,
-  imageMin,
-  compileJs,
-  compileCss,
+  parallel(
+    html,
+    js,
+    jsPlugins,
+    css,
+    cssPlugins,
+  ),
 );
 
 exports.watch = livereload;
